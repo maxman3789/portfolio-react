65193c9ca108a06468e0c2ffea0f28ad986c6381
maxman3789 <maxman3789@aol.com>
2022-03-03 21:25:22 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,55 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Navbar from "./components/Navbar";
-// import Header from "./components/Header";
-// import AboutMe from "./components/AboutMe";
-// import Portfolio from "./components/Portfolio";
-// import Contacts from "./components/Contacts";
-// import Footer from "./components/Footer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import Navbar from "./components/Navbar/index";
-import Footer from "./components/Footer/index";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-
-//     </div>
-    
-//   );
-// }
 
 function App() {
+  
   return (
-    <Router>
-      <div>
-      <Navbar />
-      <Route exact path="/" component={About} />
-      <Route exact path="/About" component={About} />
-      <Route exact path="/Portfolio" component={Portfolio} />
-      <Route exact path="/Contact" component={Contact} />
-      <Footer />
-      </div>
-    </Router>
+    <div className="App">
+    <PortfolioContainer />
+
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>  
+      
+
+    </div>
+    
   );
 }
+
 
 export default App;
